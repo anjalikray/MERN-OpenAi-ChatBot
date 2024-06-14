9ca04bf707d33b1e431321db6fab08e9efa8349e
Anjali <anjalikumari2907@gmail.com>
2024-06-14 15:55:28 +0530

    login validation added

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/user.js";
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
 
 //get all users
 export const getAllUsers = async (
@@ -17,18 +17,48 @@ export const getAllUsers = async (
     }
 };
 
+//user signup
 export const userSignup = async (
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
     try {
-        //user signup
         const { name, email, password } = req.body;
-        const hashedPassword = await hash(password, 10);  //using bcypt to hash password here 10 means password will hashed in 10 rounds 
+        const existingUser = await User.findOne({ email });
+        if (existingUser)
+            return res.status(401).send("User already registered");
+
+        const hashedPassword = await hash(password, 10); //using bcypt to hash password here 10 means password will hashed in 10 rounds
         const user = new User({ name, email, password: hashedPassword });
         await user.save();
-        return res.status(201).json({ message: "OK", id:user._id.toString() });
+
+        return res.status(201).json({ message: "OK", id: user._id.toString() });
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+
+//user login
+export const userLogin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).send("User not registered");
+        }
+
+        const isPasswordCorrect = await compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).send("Incorrect Password");
+        }
+
+        return res.status(200).json({ message: "OK", id: user._id.toString() });
     } catch (error) {
         console.log(error);
         return res.status(200).json({ message: "ERROR", cause: error.message });
--- backend/src/routes/user-route.ts
+++ backend/src/routes/user-route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAllUsers, userSignup } from "../controllers/user-controllers.js";
-import { signupValidator, validate } from "../utils/validators.js";
+import { getAllUsers, userSignup , userLogin } from "../controllers/user-controllers.js";
+import { loginValidator, signupValidator, validate } from "../utils/validators.js";
 
 const userRouter = Router()
 
 userRouter.get("/" , getAllUsers)
 userRouter.post("/signup" , validate(signupValidator) , userSignup)
+userRouter.post("/login" , validate(loginValidator) , userLogin)
 
 export default userRouter;
