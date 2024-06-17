71b25bf58e779c907fdcd3303b217e3b8d210788
Anjali <anjalikumari2907@gmail.com>
2024-06-17 22:21:11 +0530

    token verification

--- backend/src/routes/user-route.ts
+++ backend/src/routes/user-route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { getAllUsers, userSignup , userLogin } from "../controllers/user-controllers.js";
 import { loginValidator, signupValidator, validate } from "../utils/validators.js";
+import { verifyToken } from "../utils/token-manager.js";
 
 const userRouter = Router()
 
 userRouter.get("/" , getAllUsers)
 userRouter.post("/signup" , validate(signupValidator) , userSignup)
 userRouter.post("/login" , validate(loginValidator) , userLogin)
+userRouter.get("/auth-status" , verifyToken , userLogin)
 
 export default userRouter;
--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -1,4 +1,6 @@
+import { NextFunction ,Request , Response, response } from "express";
 import jwt from "jsonwebtoken";
+import { COOKIE_NAME } from "./constants.js";
 
 export const createToken = (id: string, email: string, expiresIn: string) => {
     const payload = { id, email };
@@ -7,3 +9,24 @@ export const createToken = (id: string, email: string, expiresIn: string) => {
     });
     return token;
 };
+
+export const verifyToken = async (req : Request , res: Response , next: NextFunction) => {
+    const token = req.signedCookies[`${COOKIE_NAME}`]
+    if(!token || token.trim() === ""){
+        return res.status(401).json({message: "Token not recived"})
+    }
+    // console.log(token)
+    return new Promise<void>((resolve , reject) => {
+        return jwt.verify(token , process.env.JWT_SECRET , (err , success) => {
+            if(err){
+                reject(err.message)
+                return response.status(401).json({message:"Token Expired "})
+            } else {
+                console.log("Token Varification Successful")
+                resolve()
+                res.locals.jwtData = success
+                return next()
+            }
+        })
+    })
+}
