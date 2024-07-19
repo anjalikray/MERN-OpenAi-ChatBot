9330560e9e7e985315b7afedd6c8a3cb9fdcf5e5
Anjali <anjalikumari2907@gmail.com>
2024-07-19 23:07:16 +0530

    vercel config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,18 +3,24 @@ import { config } from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 
 config();
 
 const app = express();
 
 //middlewares
-app.use(cors({origin:"http://localhost:5173" , credentials: true}))
+app.use(
+    cors({
+        origin: ["https://mern-open-ai-chat-e1ntt4giy-anjali-s-projects-f5e0f9ef.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true,
+    })
+);
 app.use(express.json());
-app.use(morgan("dev"));    //remove it in production
-app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(morgan("dev")); //remove it in production
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
-app.use("/api/v1" , appRouter)
+app.use("/api/v1", appRouter);
 
-export default app; 
+export default app;
